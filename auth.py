70bf5fedbe26e0139cbea5e2a41753e0ed6df1ca
Chetan Birajdar <birajdar.chetan@gmail.com>
2017-06-13 18:58:04 -0700

    get project id from OS_TENANT_ID environment variable set from openrc config

--- auth.py
+++ auth.py
@@ -13,12 +13,12 @@ class AuthenticationAPI:
         self.user_domain_id = os.environ.get('OS_USER_DOMAIN_ID')
         self.project_id = os.environ.get('OS_TENANT_ID')
 
-    def project_scoped_session(self, project_id):
+    def project_scoped_session(self):
         auth = v3.Password(auth_url=self.url,
                            username=self.username,
                            password=self.password,
                            user_domain_id=self.user_domain_id,
-                           project_id=project_id)
+                           project_id=self.project_id)
         return session.Session(auth=auth)
 
     def domain_scoped_session(self):
@@ -32,8 +32,8 @@ class AuthenticationAPI:
     def keystone_api(self):
         return keystone.Client(session=self.domain_scoped_session())
 
-    def nova_api(self, project_id):
-        return nova.Client("2.1", session=self.project_scoped_session(project_id=project_id))
+    def nova_api(self):
+        return nova.Client("2.1", session=self.project_scoped_session())
 
-    def cinder_api(self, project_id):
-        return cinder.Client("2",session=self.project_scoped_session(project_id=project_id))
+    def cinder_api(self):
+        return cinder.Client("2",session=self.project_scoped_session())
--- examples.py
+++ examples.py
@@ -9,12 +9,13 @@ for project in keystone.projects.list():
     print project.id, project.name
 
 # Example compute service
-nova = auth.nova_api(project_id='YOUR_PROJECT_ID')
+nova = auth.nova_api()
 
 for server in nova.servers.list():
     print server.id, server.name
 
-cinder = auth.cinder_api(project_id='YOUR_PROJECT_ID')
+# Example block storage service
+cinder = auth.cinder_api()
 
 for volume in cinder.volumes.list():
     print volume.id, volume.name
